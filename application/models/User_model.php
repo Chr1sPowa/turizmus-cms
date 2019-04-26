61a51408073486483e778be61a5361d5158194ba
Chr1sPowa <Chr1sPowa@users.noreply.github.com>
2019-04-26 20:14:43 +0200

    upload

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,6 +1,7 @@
 <?php
 class User_model extends CI_Model {
     public function register($datas) {
+        $datas['Password'] = md5($datas['Password'] );
         $this->db->insert('users', $datas);
         return $this->db->insert_id();
     }
